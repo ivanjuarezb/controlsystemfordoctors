57c4759f1101ee1f30863eec13c76a4680a1d2d3
ivanjuarezb <ivanjuarezb@outlook.es>
2022-06-26 22:06:56 +0200

    Created modules and completed 'app-routing'

--- client-angular/src/app/app-routing.module.ts
+++ client-angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrearPacienteComponent } from './modules/crear-paciente/crear-paciente.component';
+import { DatosPacienteComponent } from './modules/datos-paciente/datos-paciente.component';
+import { HistorialPacienteComponent } from './modules/historial-paciente/historial-paciente.component';
+import { NuevoHistorialComponent } from './modules/nuevo-historial/nuevo-historial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'crear-paciente',     component: CrearPacienteComponent },
+  { path: 'datos-paciente',     component: DatosPacienteComponent },
+  { path: 'historial-paciente', component: HistorialPacienteComponent },
+  { path: 'nuevo-historial',    component: NuevoHistorialComponent },
+  { path: '**', redirectTo: 'crear-paciente' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './pages/header/header.component';
-import { CrearPacienteComponent } from './pages/crear-paciente/crear-paciente.component';
-import { DatosPacienteComponent } from './pages/datos-paciente/datos-paciente.component';
-import { NuevoHistorialComponent } from './pages/nuevo-historial/nuevo-historial.component';
-import { HistorialPacienteComponent } from './pages/historial-paciente/historial-paciente.component';
-import { ExpedienteComponent } from './pages/expediente/expediente.component';
+import { HeaderComponent } from './modules/header/header.component';
+import { CrearPacienteComponent } from './modules/crear-paciente/crear-paciente.component';
+import { DatosPacienteComponent } from './modules/datos-paciente/datos-paciente.component';
+import { NuevoHistorialComponent } from './modules/nuevo-historial/nuevo-historial.component';
+import { HistorialPacienteComponent } from './modules/historial-paciente/historial-paciente.component';
+import { ExpedienteComponent } from './modules/expediente/expediente.component';
 
 @NgModule({
   declarations: [
